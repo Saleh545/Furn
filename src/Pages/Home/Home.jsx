188a36d583b2f17b3915e75161ccfec41220443a
Saleh545 <syusifzade545@gmail.com>
2024-06-15 13:49:31 +0400

    main

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import Header from '../../Components/Header/Header'
+import React, { useEffect } from "react";
+import Header from "../../Components/Header/Header";
+import "./home.css";
+import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+ 
   return (
     <div>
-      
-    <Header/>
+      <Header />
+      <main>
+        <div className="slider-area">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-6 col-lg-8 col-md-8">
+                <div className="hero__caption">
+                  <span>70% Sale off </span>
+                  <h1 data-aos="fade-up">Furniture at Cost</h1>
+                  <p data-aos="fade-up">
+                    Suspendisse varius enim in eros elementum tristique. Duis
+                    cursus, mi quis viverra ornare, eros dolor interdum nulla.
+                  </p>
+                  <div className="hero-btn" data-aos="fade-up">
+                    <Link className=" hero_btn">Discover More</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'aos/dist/aos.css';  
+import AOS from 'aos';     
+AOS.init({ duration: 1000 });  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
