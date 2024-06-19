3e2f951292e1ab070a1b23268f8f158f5aaa9014
Saleh545 <syusifzade545@gmail.com>
2024-06-19 18:59:11 +0400

    finish

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./login.css"
 const Login = () => {
   return (
-    <div>Login page</div>
-  )
-}
+    <div>
+      <main className="login-bg">
+        <div className="login-form-area">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-xl-7 col-lg-8">
+                <div className="login-form">
+                  <div className="login-heading">
+                    <span>Login</span>
+                    <p>Enter Login details to get access</p>
+                  </div>
+                  <div className="input-box">
+                    <div className="single-input-fields">
+                      <label>Username or Email Address</label>
+                      <input
+                        type="text"
+                        placeholder="Username / Email address"
+                      />
+                    </div>
+                    <div className="single-input-fields">
+                      <label>Password</label>
+                      <input type="password" placeholder="Enter Password" />
+                    </div>
+                    <div className="single-input-fields login-check">
+                      <input type="checkbox" id="fruit1" name="keep-log" />
+                      <label for="fruit1">Keep me logged in</label>
+                      <a href="#" class="f-right">
+                        Forgot Password?
+                      </a>
+                    </div>
+                  </div>
+                  <div className="login-footer">
+                    <p>Don’t have an account? <Link to="/register">Sign Up</Link>here</p>
+                    <button class="submit-btn3">Login</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import "./register.css";
+import { Link } from "react-router-dom";
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <div className="login-bg">
+        <div className="register-form-area">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-xl-6 col-lg-8">
+                <div className="register-form text-center">
+                  <div className="register-heading">
+                    <span>Sign Up</span>
+                    <p>Create your account to get full access</p>
+                  </div>
+                  <div className="input-box">
+                    <div className="single-input-fields">
+                      <label>Full name</label>
+                      <input type="text" placeholder="Enter full name" />
+                    </div>
+                    <div className="single-input-fields">
+                      <label>Email Address</label>
+                      <input type="email" placeholder="Enter email adress" />
+                    </div>
+                    <div className="single-input-fields">
+                      <label>Password</label>
+                      <input type="password" placeholder="Enter Password" />
+                    </div>
+                    <div className="single-input-fields">
+                      <label>Confirm Password</label>
+                      <input type="password" placeholder="Confirm Password" />
+                    </div>
+                  </div>
+                  <div className="register-footer">
+                    <p>
+                      Already have an account? <Link> Login</Link>here
+                    </p>
+                    <button class="submit-btn3">Sign Up</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
